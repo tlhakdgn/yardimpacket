c4d924e653dd5fd8c1e9d58124a697e5b510ae3c
Talha AKDOĞANÖZÜ <115941729+tlhakdgn@users.noreply.github.com>
2023-10-05 17:12:49 +0300

    Add files via upload

---
+++ github_profilleri/script.js
@@ -0,0 +1,95 @@
+const APIURL = 'https://api.github.com/users/'
+
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username)
+
+        createUserCard(data)
+        getRepos(username)
+    } catch(err){
+        if(err.response.status == 404) {
+            createErrorCard('Bu isme sahip bir hesap bulunmamaktadır.')
+        }
+    }
+}
+
+
+async function getRepos(username) {
+    axios(APIURL + username + '/repos?sort=created').then(function(response){
+        const repos=response.data;
+        repos.forEach(function(repo){
+            console.log(repo.name);
+            const reposDiv=document.getElementById("repos");
+            reposDiv.innerHTML+="<div class='repo'>"+repo.name+"</div>";
+        })
+    }).catch(function(){
+        console.log("hata oluştu...");
+    });
+}
+
+function createUserCard(user) {
+    const userID = user.name || user.login
+    const userBio = user.bio ? `<p>${user.bio}</p>` : ''
+    const cardHTML = `
+    <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${userID}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos">
+      </div>
+    </div>
+  </div>
+    `
+    main.innerHTML = cardHTML
+    
+}
+
+function createErrorCard(msg) {
+    const cardHTML = `
+        <div class="card">
+            <h1>${msg}</h1>
+        </div>
+    `
+
+    main.innerHTML = cardHTML
+}
+
+function addReposToCard(repos) {
+    const reposEl = document.getElementById('repos')
+
+    repos
+        .slice(0, 5)
+        .forEach(repo => {
+            const repoEl = document.createElement('a')
+            repoEl.classList.add('repo')
+            repoEl.href = repo.html_url
+            repoEl.target = '_blank'
+            repoEl.innerText = repo.name
+
+            repoEl.appendChild(repoEl)
+        })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
